b69482a65f5b42b782b3ff7ae60d466bceaa1874
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-08-26 19:00:33 +0100

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let computerScore = 0
 function getComputerChoice() {
     let randNum = Math.random()
     if (randNum > 2/3) {
-        return "Rock"
+        return "rock"
     } else if (randNum > 1/3) {
-        return "Scissors"
+        return "paper"
     } else {
-        return "Paper"
+        return "scissors"
     }
 }
 
@@ -16,3 +16,39 @@ function getHumanChoice() {
     let choice = prompt("Would you like to play Rock, Paper or Scissors? ")
     return choice
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice == "rock" && computerChoice == "rock") {
+        console.log("It's a tie! Both of you played rock!")
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore += 1
+        console.log("YOU WIN! (Rock beats scissors)")
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore += 1
+        console.log("You lose. (Paper beats rock)")
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log("It's a tie! Both of you played scissors!")
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore += 1
+        console.log("You lose. (Rock beats scissors)")
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore += 1 
+        console.log("YOU WIN! (Scissors beats paper)")
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        console.log("It's a tie! Both of you played paper!")
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore += 1
+        console.log("YOU WIN! (Paper beats rock)")
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore += 1
+        console.log("You lose. (Scissors beats paper)")
+    } else {
+        return "Invalid input"
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
