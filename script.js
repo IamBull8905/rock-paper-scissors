35cb4e5b3952da71b4ad81a0d14c3a9d772fd32e
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-08-26 18:29:30 +0100

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let randNum = Math.random()
+    if (randNum > 2/3) {
+        return "Rock"
+    } else if (randNum > 1/3) {
+        return "Scissors"
+    } else {
+        return "Paper"
+    }
+}
