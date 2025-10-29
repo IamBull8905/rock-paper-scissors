1e4784f599fb7e0c495c43606f80234f22014949
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-10-29 20:05:13 +0000

    Better readability and remove unnecessary if statements
    This commit adds the semicolons, continuing from the "cleaner code" lesson as well as removing two if statements that were bloating the program in preparation for the ui.

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
 function getComputerChoice() {
-    let randNum = Math.random()
+    let randNum = Math.random();
     if (randNum > 2/3) {
-        return "rock"
+        return "rock";
     } else if (randNum > 1/3) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    let choice = prompt("Would you like to play Rock, Paper or Scissors? ")
-    return choice
+    let choice = prompt("Would you like to play Rock, Paper or Scissors? ");
+    return choice;
 }
 
 function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+    let humanScore = 0;
+    let computerScore = 0;
         function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase()
-        if (humanChoice == "rock" && computerChoice == "rock") {
-            console.log("It's a tie! Both of you played rock!")
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice == computerChoice) {
+            console.log(`It's a tie! Both of you played ${humanChoice}!`);
         } else if (humanChoice == "rock" && computerChoice == "scissors") {
-            humanScore += 1
-            console.log("YOU WIN! (Rock beats scissors)")
+            humanScore += 1;
+            console.log("YOU WIN! (Rock beats scissors)");
         } else if (humanChoice == "rock" && computerChoice == "paper") {
-            computerScore += 1
-            console.log("You lose. (Paper beats rock)")
-        } else if (humanChoice == "scissors" && computerChoice == "scissors") {
-            console.log("It's a tie! Both of you played scissors!")
+            computerScore += 1;
+            console.log("You lose. (Paper beats rock)");
         } else if (humanChoice == "scissors" && computerChoice == "rock") {
-            computerScore += 1
-            console.log("You lose. (Rock beats scissors)")
+            computerScore += 1;
+            console.log("You lose. (Rock beats scissors)");
         } else if (humanChoice == "scissors" && computerChoice == "paper") {
-            humanScore += 1 
-            console.log("YOU WIN! (Scissors beats paper)")
-        } else if (humanChoice == "paper" && computerChoice == "paper") {
-            console.log("It's a tie! Both of you played paper!")
+            humanScore += 1; 
+            console.log("YOU WIN! (Scissors beats paper)");
         } else if (humanChoice == "paper" && computerChoice == "rock") {
-            humanScore += 1
-            console.log("YOU WIN! (Paper beats rock)")
+            humanScore += 1;
+            console.log("YOU WIN! (Paper beats rock)");
         } else if (humanChoice == "paper" && computerChoice == "scissors") {
-            computerScore += 1
-            console.log("You lose. (Scissors beats paper)")
+            computerScore += 1;
+            console.log("You lose. (Scissors beats paper)");
         } else {
-            return "Invalid input"
+            return "Invalid input";
         }
     }
     for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
-    console.log(`The computer scored: ${computerScore}`)
-    console.log(`You scored ${humanScore}`)
+    console.log(`The computer scored: ${computerScore}`);
+    console.log(`You scored ${humanScore}`);
 }
 
-playGame()
+playGame();
